cf6ff28f14f1fd2d0a17d7b0619dc2042f0539f4
Roman <r.yurlov@promo-bot.ru>
2020-04-11 21:36:19 +0500

    add %

--- controllers/getCountBot.js
+++ controllers/getCountBot.js
@@ -4,10 +4,8 @@ module.exports.getCountBot = async function getCountBot (ctx, next) {
   const { data } = ctx.request.body
   if (!data) return ctx.throw(400, 'Something go wrong')
   if (data !=='91-DiVoC-TsEt') return ctx.throw(400, 'Something go wrong')
-
   const count = await Data.find()
   const conuntBot = count.filter(item => item.bot)
-
   ctx.status = 200
   ctx.body = {
     data: conuntBot.length
