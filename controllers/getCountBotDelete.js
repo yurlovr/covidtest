3f38f5d22814ec99aafa99c256d9d35c66858332
Roman <r.yurlov@promo-bot.ru>
2020-04-11 22:46:24 +0500

    fix

--- controllers/getCountBotDelete.js
+++ controllers/getCountBotDelete.js
@@ -5,6 +5,7 @@ module.exports.getCountBotDelete = async function getCountBotDelete (ctx, next) 
   if (!data) return ctx.throw(400, 'Something go wrong')
   if (data !=='91-DiVoC-TsEt') return ctx.throw(400, 'Something go wrong')
   await Data.deleteMany({bot: true})
+
   ctx.status = 200
   ctx.body = {
     data: 'success'
